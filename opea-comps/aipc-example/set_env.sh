e17a63c69770bae5f257c5353192ad6f5e5bff9b
vennieso <vennie.so@gmail.com>
2025-03-03 22:28:21 +0800

    #4 changes to run on mac

--- opea-comps/aipc-example/set_env.sh
+++ opea-comps/aipc-example/set_env.sh
@@ -3,11 +3,19 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-pushd "../../../../../" > /dev/null
-source .set_env.sh
-popd > /dev/null
+# pushd "../../../../../" > /dev/null
 
-export host_ip=$(hostname -I | awk '{print $1}')
+# # Check if .set_env.sh exists before sourcing
+# if [ -f ".set_env.sh" ]; then
+#     source .set_env.sh
+# else
+#     echo "Error: .set_env.sh not found."
+#     exit 1
+# fi
+
+# popd > /dev/null
+
+export host_ip=$(hostname | awk '{print $1}')
 
 if [ -z "${HUGGINGFACEHUB_API_TOKEN}" ]; then
     echo "Error: HUGGINGFACEHUB_API_TOKEN is not set. Please set HUGGINGFACEHUB_API_TOKEN."
@@ -22,7 +30,7 @@ export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
 export RERANK_MODEL_ID="BAAI/bge-reranker-base"
 export INDEX_NAME="rag-redis"
 export OLLAMA_HOST=${host_ip}
-export OLLAMA_MODEL="llama3.2"
+export OLLAMA_MODEL="llama3.2:1b"
 # Set it as a non-null string, such as true, if you want to enable logging facility,
 # otherwise, keep it as "" to disable it.
 export LOGFLAG=""
